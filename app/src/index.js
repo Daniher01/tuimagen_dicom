8dffd776877b27749c35777326ef9e4b00f4cb72
Daniel Hernandez <daniher02@gmail.com>
2023-11-22 11:29:13 -0300

    update depues de mucho tiempo

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const multer = require('multer');
+const multer = require('multer');
 const axios = require('axios');
 
 const app = express();
@@ -7,35 +7,38 @@ app.use(express.static('public'));
 
 // ? Variables de entorno
 const PORT = 8000
-const API_URL = process.env.API_URL || 'http://192.168.1.114:3000/orthanc'
+const API_URL = process.env.API_URL || 'http://localhost:3000/orthanc'
+
+
+// ? MULTER
+// Configuración de multer para procesar archivos
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/index.html'); 
 })
 
 // TODO revisar la subida de archivos
-app.post('/upload', (req, res) => {
-    const file = req.file; // ? variable
-
-    // ? envio de archivo a la api rest
-    const formData = new FormData();
-    formData.append('file', file.buffer, { filename: file.originalname });
-
-    axios.post(`${API_URL}/studies`, formData, {
-        headers: {
-          ...formData.getHeaders(),
-          //Authorization: 'Bearer ' + token // si es necesario autenticarse en la API REST
-        }
-      })
-      .then(response => {
-        console.log(response.data);
-        res.send(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-        res.status(500).send('Ocurrió un error al subir el archivo');
-      });
-  });
+// Manejador de la solicitud POST para subir estudios a Orthanc
+app.post('/upload', upload.single('study'), async (req, res) => {
+  const studyFile = req.file.buffer; // Obtiene el archivo ZIP del campo "study"
+  const url = `${API_URL}/instances`; // URL de la API REST de Orthanc
+  console.log('Enviando Archivo ...');
+  try {
+    // Envía el archivo ZIP a la API REST de Orthanc utilizando axios
+    const response = await axios.post(url, studyFile, {
+      headers: {
+        'Content-Type': 'application/zip'
+      }
+    });
+    console.log('Estudio enviado correctamente a Orthanc');
+    res.send(response);
+  } catch (error) {
+    console.error(error);
+    res.send(error);
+  }
+});
 
 app.get('/studies/:uid', (req, res) => {
     const uid = req.params.uid
@@ -45,11 +48,13 @@ app.get('/studies/:uid', (req, res) => {
       })
       .catch(error => {
         console.log(error);
-        res.send(error);
+        res.send(data); 
       });
 })  
 
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Servidor iniciado en el puerto ${PORT}.`);
   });
+
+server.timeout = 30000;
